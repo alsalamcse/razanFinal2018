de9dd464b5c718b6ab96ad3a8df12a9c9de209a6
user <rimaa@danon.tzafonet.org.il>
2018-11-15 14:49:13 +0200

    1.splash screen is ok, and added main activity TABS 2.next step: buildin login, sign up screens

--- app/src/main/java/com/hammoud/razan/razanfinal2018/LogInActivity.java
+++ app/src/main/java/com/hammoud/razan/razanfinal2018/LogInActivity.java
@@ -1,15 +1,22 @@
 package com.hammoud.razan.razanfinal2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LogInActivity extends AppCompatActivity {
     private EditText etEmail1, etPassword1;
     private Button btnSignIn, btnSignUp;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,15 @@ public class LogInActivity extends AppCompatActivity {
         etPassword1=findViewById(R.id.etPassward1);
         btnSignIn=findViewById(R.id.btnSignIn);
         btnSignUp=findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dataHundler();
+            }
+        });
+
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +50,41 @@ public class LogInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void dataHundler() {
+        boolean isOk= true; // if alla the fields filled well
+        String email= etEmail1.getText().toString();
+        String password= etPassword1.getText().toString();
+        boolean isok = false;
+        if (email.length()<4 || email.indexOf('@')<0 || email.indexOf('.')<0)
+        {
+            etEmail1.setError("worng Email");
+            isok=false;
+        }
+        if (password.length()<8)
+        {
+            etPassword1.setError(" Have to be at least 8 char");
+            isok=false;
+        }
 
+    }
+    private void signIn(String email, String passw){
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email,passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful())
+                {
+                    Intent i = new Intent(LogInActivity.this,LogInActivity.class);
+                    startActivity(i);
+                    Intent intent = new Intent(LogInActivity.this,SignUpActivity.class);
+                    startActivity(i);
+
+                }
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/hammoud/razan/razanfinal2018/SignUpActivity.java
+++ app/src/main/java/com/hammoud/razan/razanfinal2018/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.hammoud.razan.razanfinal2018;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -80,6 +80,13 @@ public class SignUpActivity extends AppCompatActivity {
                     task.getException().printStackTrace();
 
                 }
+            }
+        });
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUpActivity.this,LogInActivity.class);
+                startActivity(i);
             }
         });
     }
